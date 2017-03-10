98b0d1c7ee3ee022df5d0387a7b06f0d809ad012
PlanetEfficacy <jspevack@gmail.com>
2017-03-10 13:17:39 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# printreleaf-javascript
---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('A basic test', () => {
+  it('should pass when everythying is okay', () => {
+    expect(true).to.be.true;
+  })
+})
