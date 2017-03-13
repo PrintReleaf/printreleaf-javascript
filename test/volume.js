1067c7c36001e92c78a64875923a2456567c0d23
PlanetEfficacy <jspevack@gmail.com>
2017-03-13 17:24:29 -0600

    Add volume #list

---
+++ test/volume.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const printreleaf = require('./testUtils').getSpyablePrintReleaf();
+
+describe('Volume', () => {
+
+  describe('#list', () => {
+    it('yields a list of all volume history summarized by period', () => {
+      printreleaf.volume.list({ period: 'monthly' });
+      expect(printreleaf.LAST_REQUEST).to.deep.equal({
+        method: 'GET',
+        url: '/volume',
+        data: { period: 'monthly' }
+      });
+    });
+  });
+
+});
