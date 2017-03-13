ff541468ce8735a95a0b081a634b860897525cf3
PlanetEfficacy <jspevack@gmail.com>
2017-03-13 16:33:01 -0600

    Add list, get, create, create, delete for invitations

--- test/invitations_spec.js
+++ test/invitations_spec.js
@@ -1,48 +1,48 @@
 const expect = require('chai').expect;
 const printreleaf = require('./testUtils').getSpyablePrintReleaf();
 
-describe('Deposits', () => {
+describe('Invitations', () => {
   describe('#list', () => {
-    it('yields a list of all deposits', () => {
-      printreleaf.deposits.list({ page: 5 });
+    it('yields a list of all invitations', () => {
+      printreleaf.invitations.list({ page: 5 });
       expect(printreleaf.LAST_REQUEST).to.deep.equal({
         method: 'GET',
-        url: '/deposits',
+        url: '/invitations',
         data: { page: 5 }
       });
     });
   });
   
   describe('#find', () => {
-    it('yields the deposits assoicated with the given id', () => {
-      printreleaf.deposits.find('pants1234');
+    it('yields the invitations assoicated with the given id', () => {
+      printreleaf.invitations.find('pants1234');
       expect(printreleaf.LAST_REQUEST).to.deep.equal({
         method: 'GET',
-        url: '/deposits/pants1234',
+        url: '/invitations/pants1234',
         data: {}
       });
     });
   });    
 
   describe('#create', () => {
-    it('yields the newly created deposits', () => {
+    it('yields the newly created invitations', () => {
       const data = { name: 'pants' };
-      printreleaf.deposits.create(data);
+      printreleaf.invitations.create(data);
       expect(printreleaf.LAST_REQUEST).to.deep.equal({
         method: 'POST',
-        url: '/deposits',
+        url: '/invitations',
         data: data 
       });
     });
   });    
 
   describe('#delete', () => {
-    it('yields the newly deleted deposits', () => {
+    it('yields the newly deleted invitations', () => {
       const id = 'pants1234'; 
-      printreleaf.deposits.delete(id);
+      printreleaf.invitations.delete(id);
       expect(printreleaf.LAST_REQUEST).to.deep.equal({
         method: 'DELETE',
-        url: '/deposits/pants1234',
+        url: '/invitations/pants1234',
         data: {}
       });
     });
